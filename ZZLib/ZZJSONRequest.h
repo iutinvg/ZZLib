e0e124726f5e919d6953494e39569cc854b50994
Vyacheslav Iutin <iutin@whirix.com>
2011-04-09 11:26:19 +0700

    Documentation, initial steps.

--- ZZLib/ZZJSONRequest.h
+++ ZZLib/ZZJSONRequest.h
@@ -4,7 +4,9 @@
  */
 
 /**
- * Wrapper for NSURLConnection for handful async getting of JSON data.
+ Wrapper for NSURLConnection for handful async getting of JSON data.
+ 
+ Usually it is used in pair with ZZTableController or ZZViewController.
  */
 
 #import <Foundation/Foundation.h>
@@ -20,13 +22,47 @@
 	id<ZZJSONRequestDelegate> _delegate;
 }
 
+
 @property (nonatomic, copy) NSString* urlString;
+
+/** 
+ Response data.
+ 
+ You can use it when loading is finished.
+ Depending on the JSON you get it can be NSArray or NSDictionary.
+ Just use casting to access it:
+ 
+ `NSArray* items = (NSArray*)self.request.response;
+ NSArray* a = a;`
+ 
+ @warning You must retain it for futher usage in your methods.
+ */
 @property (nonatomic, retain) id response;
+
+/** Current connection used for loading. */
 @property (nonatomic, retain) NSURLConnection* connection;
+
+/** Indicates the loading is finished. */
 @property (nonatomic, assign) BOOL loaded;
+
+/** Indicates the loading in process. */
 @property (nonatomic, assign) BOOL loading;
 
+/**
+ Creates request with given URL and delegate.
+ 
+ The request starts loading immediatelly.
+ @param urlstr URL string to load data from
+ @param delegate NSURLConnection delegate
+*/
 - (id)initWithURLString:(NSString*)urlstr delegate:(id<ZZJSONRequestDelegate>)delegate;
+
+/**
+ Cancels loading of request.
+ 
+ You have to call it when close the screen before data is loaded.
+ However, ZZTableController calls it properly at such cases.
+ */
 - (void)cancel;
 
 @end
--- ZZLib/ZZJSONRequestDelegate.h
+++ ZZLib/ZZJSONRequestDelegate.h
@@ -10,9 +10,35 @@
 
 @class ZZJSONRequest;
 
+/**
+ Delegate to handle request async loading.
+ 
+ It must be used for controller which uses ZZJSONRequest.
+ @see ZZTableController
+ */
 @protocol ZZJSONRequestDelegate <NSObject>
 
+/**
+ Invocated when loading is failed with error.
+ 
+ This method allows to show error alerts, disable loading visualizing. 
+ @warning You must release request here.
+ 
+ @param request the request used for loading
+ @param error the failure reason information
+ */
 - (void)request:(ZZJSONRequest*)request failedWithError:(NSError*)error;
+
+/**
+ Invocated when loading successfuly finished.
+ 
+ This method allows to show error alerts, disable loading visualizing.
+ @warning You must release request here.
+
+ @param request the request used for loading
+ 
+ @see ZZJSONRequest.request
+ */
 - (void)requestDidFinishLoading:(ZZJSONRequest*)request;
 
 @end
