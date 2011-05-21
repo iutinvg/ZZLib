9ecded4b91b813ce90dc0984c9a2f0d64ee57a19
Vyacheslav Iutin <iutin@whirix.com>
2011-05-21 11:09:56 +0700

    comments for ZZViewController; slight coding style changes in Demo

--- ZZLib/ZZDebug.h
+++ ZZLib/ZZDebug.h
@@ -4,21 +4,29 @@
  */
 
 /**
- * Debugging tools.
- *
- * ZZLOG(@"formatted log text %d", param1);
- * Print the given formatted text to the log.
- *
- * Tips:
- * - to enable logging just define DEBUG macros for Debug configuration 
- *   of your project: select a Target, click Info button, select Build tab,
- *   select Debug configuration, find Preprocessor Macros, add DEBUG
- * - thus logging will work for Debug configuration, Release/Distribution 
- *   configurations will be silent; if you'd like to log something in
- *   all configurations then just use NSLog()
+ @name Debugging Tool
  */
 
-// The general purpose logger. This ignores logging levels.
+/**
+ Logger.
+ 
+ ZZLOG(@"formatted log text %d", param1);
+ Print the given formatted text to the log.
+ 
+ Example:
+    // output some variables
+    ZZLOG(@"str: %@, float: %f, int: %d", strVal, floatVal, intVal);
+    // output rectangle
+    ZZLOG(@"frame: %@", NSStringFromCGRect(rect));
+ 
+ Tips:
+ - to enable logging just define DEBUG macros for Debug configuration 
+   of your project: select a Target, click Info button, select Build tab,
+   select Debug configuration, find Preprocessor Macros, add DEBUG
+ - thus logging will work for Debug configuration, Release/Distribution 
+   configurations will be silent; if you'd like to log something in
+   all configurations then just use NSLog()
+ */
 #ifdef DEBUG
 	#define ZZLOG(xx, ...)  NSLog(@"%s(%d): " xx, __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
 #else
--- ZZLib/ZZViewController.h
+++ ZZLib/ZZViewController.h
@@ -3,14 +3,51 @@
  * License: http://www.opensource.org/licenses/mit-license.html
  */
 
-/**
- * Short hand for creation of view controller with white view stretched to 
- * full screen and with support of all orientations.
+/** 
+ Short hand for creation of view controller with white view stretched to 
+ full screen and with support of all interface orientations.
+ 
+ Please see Demo part of ZZLib project as a best example of the usage: 
+ 
+ - https://github.com/iutinvg/ZZLib/blob/master/Demo/ZZDemoViewController.h
+ - https://github.com/iutinvg/ZZLib/blob/master/Demo/ZZDemoViewController.m
  */
 @interface ZZViewController : UIViewController {
+    /**
+     The view instance which is used for in [ZZViewController showLoading:]
+     */
 	UIView* _loadingView;
 }
 
+/** 
+ Creates/removes a view for visualizing of loading process.
+ 
+ You will need it in case of usage this class in pair with ZZJSONRequest or similar.
+ Default implementation is very simple: white background with activity indicator in the center.
+ Please override if necessary. It is not necessary to involve the super class method in your one,
+ though you may of course.
+ 
+ Override example:
+    - (void)showLoading:(BOOL)flag {
+        if (flag) {
+            _loadingView = [[UIView alloc] init]; // create a custom view due to design
+            [self.view addSubview:_loadingView];
+            return;
+        }
+        // "NO" branch just removes/releases _loadingView from self.view
+        // we can reuse it
+        [super showLoading:flag];
+    }
+ 
+ Usage example:
+    - (void)viewDidLoad {
+        [super viewDidLoad];
+        // starting load loading process here
+        [self showLoading:YES];
+    }
+ 
+ @param flag `YES` to creates loading view, `NO` to remove
+ */
 - (void)showLoading:(BOOL)flag;
 
 @end
