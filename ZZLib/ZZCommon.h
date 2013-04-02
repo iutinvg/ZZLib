ffb511b42030884f430147d96083b2dfe995e8b6
iutinvg <iutin@whirix.com>
2013-04-02 09:29:48 +0700

    iphone 5 detection function

--- ZZLib/ZZCommon.h
+++ ZZLib/ZZCommon.h
@@ -1,8 +1,3 @@
-/* 
- * Copyright (c) 2011 Whirix <info@whirix.com>
- * License: http://www.opensource.org/licenses/mit-license.html
- */
-
 /**
  Check if current device is iPad.
  @return `YES` if it is iPad, `NO` otherwise.
@@ -15,6 +10,16 @@ BOOL ZZIsPad();
  */
 BOOL ZZIsRetina();
 
+// idea is stallen from
+// http://stackoverflow.com/questions/12552856/how-to-check-iphone-device-version-in-ios/13068238#13068238
+// usage:
+// http://stackoverflow.com/questions/12395200/how-to-develop-or-migrate-apps-for-iphone-5-screen-resolution/15631256#15631256
+#define HEIGHT_IPHONE_5 568
+/**
+ Check if current device is iPhone5.
+ @return `YES` if it is iPhone5, `NO` otherwise.
+ */
+BOOL ZZIs5();
+
 // short hand for color creation
 #define ZZRGBA(r,g,b,a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:a]
-
--- ZZLib/ZZCommon.m
+++ ZZLib/ZZCommon.m
@@ -1,16 +1,16 @@
-/* 
- * Copyright (c) 2011 Whirix <info@whirix.com>
- * License: http://www.opensource.org/licenses/mit-license.html
- */
-
 #import "ZZCommon.h"
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-BOOL ZZIsPad() {
+BOOL ZZIsPad()
+{
 	return [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad;
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-BOOL ZZIsRetina() {
+BOOL ZZIsRetina()
+{
     return ([[UIScreen mainScreen] respondsToSelector:@selector(scale)] && [[UIScreen mainScreen] scale] == 2);
+}
+
+BOOL ZZIs5()
+{
+    return ([[UIScreen mainScreen] bounds].size.height == HEIGHT_IPHONE_5);
 }
