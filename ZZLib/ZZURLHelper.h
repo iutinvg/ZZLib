affd01e3accf2df406e0ea403e609f2146eba5be
iutinvg <iutin@whirix.com>
2013-06-12 19:49:25 +0700

    url helper docs - very briefly

--- ZZLib/ZZURLHelper.h
+++ ZZLib/ZZURLHelper.h
@@ -1,6 +1,23 @@
+/**
+ A collection of functions to create URLs.
+ 
+ Workflow:
+ - set base URL (in application delegate)
+ - optionally set persistent parameters to use in every request (useful of you use some
+   sort of API where you always need to pass a secret-key or something)
+ - set parameters for particular request
+ - generate URL
+ - use the URL to make request
+ */
 @interface ZZURLHelper : NSObject
 
 + (void)startWithBaseURL:(NSString*)url persistentParams:(NSDictionary*)params;
+
+/**
+ Sets parameters which will be added to query string part of all URLs.
+ 
+ @param params dictionary to use as key-value pair in query string.
+ */
 + (NSDictionary*)persistentParams:(NSDictionary*)params;
 + (NSString*)urlForMethod:(NSString*)method params:(NSDictionary*)dict;
 + (NSString*)urlForMethod3:(NSString*)action params:(NSDictionary*)dict;
