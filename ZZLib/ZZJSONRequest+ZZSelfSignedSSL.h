b85141d21c39d44aa1bfb5e518ee39bc06fc43d3
iutinvg <iutin@whirix.com>
2014-01-09 15:42:24 +0700

    helper to use self-signed certificate for your REST API

---
+++ ZZLib/ZZJSONRequest+ZZSelfSignedSSL.h
@@ -0,0 +1,13 @@
+//
+//  ZZJSONRequest+ZZSelfSignedSSL.h
+//  PhoneaPlumber
+//
+//  Created by Slava Iutin on 1/9/14.
+//  Copyright (c) 2014 Phone a Plumber. All rights reserved.
+//
+
+#import "ZZJSONRequest.h"
+
+@interface ZZJSONRequest (ZZSelfSignedSSL)
+
+@end
---
+++ ZZLib/ZZJSONRequest+ZZSelfSignedSSL.m
@@ -0,0 +1,13 @@
+//
+//  ZZJSONRequest+ZZSelfSignedSSL.m
+//  PhoneaPlumber
+//
+//  Created by Slava Iutin on 1/9/14.
+//  Copyright (c) 2014 Phone a Plumber. All rights reserved.
+//
+
+#import "ZZJSONRequest+ZZSelfSignedSSL.h"
+
+@implementation ZZJSONRequest (ZZSelfSignedSSL)
+
+@end
