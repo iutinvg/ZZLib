2473115612e0e35522c53e296a59016d3ab68d50
iutinvg <iutin@whirix.com>
2014-01-20 16:24:16 +0700

    workaround for self signed SSL connection if DEBUG defined only

--- ZZLib/ZZJSONRequest+ZZSelfSignedSSL.h
+++ ZZLib/ZZJSONRequest+ZZSelfSignedSSL.h
@@ -3,6 +3,8 @@
  * License: http://www.opensource.org/licenses/mit-license.html
  */
 
+#ifdef DEBUG
+
 #import "ZZJSONRequest.h"
 
 #warning !!! I SWEAR SHOULD NOT COMPILE IT FOR PRODUCTION, JUST REMOVE REFERENCES TO THESE FILES BEFORE PUBLISHING  !!!
@@ -10,3 +12,5 @@
 @interface ZZJSONRequest (ZZSelfSignedSSL)
 
 @end
+
+#endif
