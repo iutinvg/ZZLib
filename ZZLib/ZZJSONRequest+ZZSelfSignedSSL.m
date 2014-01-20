eb74337513d8d5fa85523ea790b326dcde7f1372
iutinvg <iutin@whirix.com>
2014-01-20 19:05:59 +0700

    remove stupid debug info

--- ZZLib/ZZJSONRequest+ZZSelfSignedSSL.m
+++ ZZLib/ZZJSONRequest+ZZSelfSignedSSL.m
@@ -10,17 +10,13 @@
 @implementation ZZJSONRequest (ZZSelfSignedSSL)
 
 - (BOOL)connection:(NSURLConnection *)connection canAuthenticateAgainstProtectionSpace:(NSURLProtectionSpace *)protectionSpace {
-    ZZLOG(@"------------------------------------898 67987 9 798 7098 7098 709 87098 7098709 808 7");
     return [protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust];
 }
 
 - (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
-    ZZLOG(@"------------------------------------898 67987 9 798 7098 7098 709 87098 7098709 808 7");
     [challenge.sender useCredential:[NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust]
          forAuthenticationChallenge:challenge];
     [challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
-    
-    ZZLOG(@"------------------------------------898 67987 9 798 7098 7098 709 87098 7098709 808 7");
 }
 
 @end
