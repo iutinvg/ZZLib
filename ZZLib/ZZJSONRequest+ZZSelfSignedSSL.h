3acf9591b38b406454b74346b0c25b7075f7a87e
iutinvg <iutin@whirix.com>
2014-02-06 11:05:13 +0700

    support ADHOC macro to compile self signed certs workaround

--- ZZLib/ZZJSONRequest+ZZSelfSignedSSL.h
+++ ZZLib/ZZJSONRequest+ZZSelfSignedSSL.h
@@ -3,14 +3,20 @@
  * License: http://www.opensource.org/licenses/mit-license.html
  */
 
-#ifdef DEBUG
-
 #import "ZZJSONRequest.h"
 
-#warning ACCEPT INSECURE SERVER CONNECTIONS, ENSURE IT IS DISABLED FOR PRODUCTION
-
+/**
+ Workaround for SSL connections with self-signed certificates which are going to use
+ on your staging servers.
+ 
+ While you are on development stage, you can safely ignore the warning. However, it
+ reminds you to take care of disabling the workaround when you go on production.
+ 
+ Adhoc builds need this workaround too. Though they usually are built without `DEBUG` macro,
+ but you can define macro `ADHOC` which will enable it too.
+ */
 @interface ZZJSONRequest (ZZSelfSignedSSL)
 
 @end
 
-#endif
+//#endif
--- ZZLib/ZZJSONRequest+ZZSelfSignedSSL.m
+++ ZZLib/ZZJSONRequest+ZZSelfSignedSSL.m
@@ -3,11 +3,13 @@
  * License: http://www.opensource.org/licenses/mit-license.html
  */
 
-#ifdef DEBUG
-
 #import "ZZJSONRequest+ZZSelfSignedSSL.h"
 
 @implementation ZZJSONRequest (ZZSelfSignedSSL)
+
+#if defined(DEBUG) || defined(ADHOC)
+
+#warning ACCEPT INSECURE SERVER CONNECTIONS, ENSURE IT IS DISABLED FOR PRODUCTION
 
 - (BOOL)connection:(NSURLConnection *)connection canAuthenticateAgainstProtectionSpace:(NSURLProtectionSpace *)protectionSpace {
     return [protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust];
@@ -19,6 +21,7 @@
     [challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
 }
 
+#endif
+
 @end
 
-#endif
